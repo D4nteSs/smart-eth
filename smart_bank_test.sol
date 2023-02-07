06a01dd8d2b35127647152ad7a835663d7017688
Alex Nikitin <81376861+D4nteSs@users.noreply.github.com>
2023-02-07 10:16:44 +0300

    Create smart_bank_test.sol

---
+++ smart_bank_test.sol
@@ -0,0 +1,32 @@
+//Test smartcontract for testNetwork Ropsten
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Bank {
+    address owner;
+    mapping(address => uint) balances;
+    
+    function Bank() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        if (balances[msg.sender] >= amount) {
+            balances[msg.sender] -= amount;
+            msg.sender.transfer(amount);
+        }
+    }
+
+    function getMyBalance() public view returns(uint) {
+        return balances[msg.sender];
+    }
+
+    function kill() public {
+        if (msg.sender == owner)
+            selfdestruct(owner);
+    }
+}
